6e8896e338979c81130104751db7b702393b9dcd
Sylvain Ducamp <sylducam@z4r1p5.42lyon.fr>
2021-05-03 08:23:15 +0200

    l

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include "../get_next_line.h"
+#include "get_next_line.h"
 
 int main()
 {
 	int	ret = 1;
-	char *line;
+	char *line = NULL;
 	int	fd = open("file", O_RDONLY);
 	while (ret == 1)
 	{
