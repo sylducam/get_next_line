af3631cace35789d407a9a4ba9091f70681e131d
Sylvain Ducamp <sylducam@z4r1p5.42lyon.fr>
2021-05-04 09:16:33 +0200

    malloc done better in gnl_strdup and gnl_strjoin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: sylducam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 23:14:45 by sylducam          #+#    #+#             */
-/*   Updated: 2021/05/04 08:51:00 by sylducam         ###   ########lyon.fr   */
+/*   Updated: 2021/05/04 09:04:36 by sylducam         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ char	*gnl_strdup(char *s)
 	str = NULL;
 	if (!s)
 		return (NULL);
-	str = (char *)malloc(sizeof(char) * (gnl_strlen(s) + 1));
+	str = (char *)malloc(sizeof(char) * (gnl_strlen(s)) + 1);
 	if (str == NULL)
 		return (NULL);
 	i = 0;
@@ -55,7 +55,7 @@ char	*gnl_strjoin(char *s1, char *s2)
 	j = 0;
 	if (s1 == NULL)
 		return (gnl_strdup(s2));
-	str = (char *)malloc(sizeof(char) * (gnl_strlen(s1) + gnl_strlen(s2) + 1));
+	str = (char *)malloc(sizeof(char) * (gnl_strlen(s1) + gnl_strlen(s2)) + 1);
 	if (str == NULL)
 		return (NULL);
 	while (s1[j])
